a4c8c4936a2aab019f9c6e4961a686b80935cd7f
branweb1 <branweb1@gmail.com>
2015-08-16 21:29:08 -0700

    map syncs with api results on results page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,10 +6,17 @@ $(document).ready(function(){
     var map = L.mapbox.map("map", "grouptwo.e32d16b4");
     map.setView([47.6097,-122.3331], 12);
 
+
+// add empty layer to map
     var foursquarePlaces = L.layerGroup().addTo(map);
 
+// grab container for search results
     var listings = document.querySelector(".listings");
 
+
+// loop through results and create map markers, add to map
+// note the venue name is stored in the marker both for accessability
+// and to grab later on when creating the list of results
     for (var i = 0; i < window.app.response.venues.length; i++) {
       var venue = window.app.response.venues[i];
       var latlng = L.latLng(venue.location.lat, venue.location.lng);
@@ -18,34 +25,29 @@ $(document).ready(function(){
             'marker-color': '#800000',
             'marker-symbol': 'marker',
             'marker-size': 'medium'
-          })
+          }), 'title': venue.name, 
         })
       .bindPopup('<strong><a href="https://foursquare.com/v/' + venue.id + '">' +
         venue.name + '</a></strong>')
         .addTo(foursquarePlaces);
-
-      // var listing = listings.appendChild(document.createElement('div'));
-      // listing.className = 'item';
-
-      // var link = listing.appendChild(document.createElement('a'));
-      // link.href = '#';
-      // link.className = 'title';
-      // link.innerHTML = venue.name;
-
     }
 
+// loop through layeers, grabing info from each layer and building
+// a list item with it
     foursquarePlaces.eachLayer(function(layer) {
-      // var listing = listings.appendChild(document.createElement('div'));
-      // listing.className = 'item';
-      // var link = listing.appendChild(document.createElement('a'));
-      // link.href = '#';
-      // link.className = 'title';
-      // link.innerHTML = venue.name;
-      // link.onclick = function() {
-      //        map.setView(layer.getLatLng(), 14);
-      //        layer.openPopup();
-      //     };
-      console.log(layer)
+      var listing = listings.appendChild(document.createElement('div'));
+      listing.className = 'item';
+      var link = listing.appendChild(document.createElement('a'));
+      link.href = '#';
+      link.className = 'title';
+      link.innerHTML = layer._icon.title;
+      link.onclick = function() {
+             map.setView(layer.getLatLng(), 14);
+             layer.openPopup();
+             // kills the link functionality so it doesn't just to the top
+             // of the page
+             return false;
+          };
       });
 
 
