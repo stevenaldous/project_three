dd45ef6a9727cf3892d15bc614eec301cdacb9a3
stevenaldous <stevenaldous@gmail.com>
2015-08-17 15:29:17 -0700

    added views and controller for dates. nav bar too.

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 require('express-helpers')(app);
 var request = require('request');
-var results = require('./models/4square.json')
+var session = require('express-session');
+var flash = require('connect-flash');
+var results = require('./models/4square.json');
+app.use(flash());
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(ejsLayouts);
-
+app.use('/users', require('./controllers/users.js'));
+app.use('/dates', require('./controllers/dates.js'));
 app.set("layout extractScripts", true);
-
 app.use(express.static('assets'));
 
+
+//////////////////////////////////////////////////////////
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('main/index');
 });
 
+app.get('/about', function(req, res){
+  res.render('main/about');
+});
+
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 app.get('/search', function(req,res) {
   res.render('search');
 });
@@ -37,5 +49,8 @@ app.get('/results', function(req,res) {
     res.render('results', { results: JSON.parse(data) });
   });
 });
-
-app.listen(process.env.PORT || 3000);
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+app.listen(process.env.PORT || 3000, function(){
+  console.log("You're lisening to the smooth sounds of Port 3000")
+});
