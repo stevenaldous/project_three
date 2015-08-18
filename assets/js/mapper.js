db475a5c14821b932bcf972abd059329d6acdc25
scruffmuffler <sloan.coffin@gmail.com>
2015-08-18 02:11:07 -0700

    map now refreshes on search

--- assets/js/mapper.js
+++ assets/js/mapper.js
@@ -1,11 +1,10 @@
  $(document).ready(function(){
-
+var map = undefined;
 function drawMap(mapData){
-
 
       L.mapbox.accessToken = "pk.eyJ1IjoiZ3JvdXB0d28iLCJhIjoiOTYyMjYwM2ExYjU0" +
                              "MTNlNzMwMmYxZDhmNTNlMzBiZDIifQ.uILo4IfMpqra-O-NpKkbqw";
-      var map = L.mapbox.map("map", "grouptwo.e32d16b4");
+      map = L.mapbox.map("map", "grouptwo.e32d16b4");
       map.setView([47.6097,-122.3331], 12);
 
 
@@ -38,35 +37,54 @@ function drawMap(mapData){
 
   // loop through layeers, grabing info from each layer and building
   // a list item with it
-      foursquarePlaces.eachLayer(function(layer) {
-        // console.log(layer);
-        var listing = listings.appendChild(document.createElement('div'));
-        listing.className = 'item';
-        var link = listing.appendChild(document.createElement('a'));
-        link.href = '#';
-        link.className = 'title';
-        link.innerHTML = layer._icon.title;
-        link.onclick = function() {
-              console.log('item data',mapData.response.venues[layer.options.idx])
-               map.setView(layer.getLatLng(), 14);
-               layer.openPopup();
-               // kills the link functionality so it doesn't just to the top
-               // of the page
-               return false;
-            };
-        });
-
+    //   foursquarePlaces.eachLayer(function(layer) {
+    //     // console.log(layer);
+    //     var listing = listings.appendChild(document.createElement('div'));
+    //     listing.className = 'item';
+    //     var link = listing.appendChild(document.createElement('a'));
+    //     link.href = '#';
+    //     link.className = 'title';
+    //     link.innerHTML = layer._icon.title;
+    //     link.onclick = function() {
+    //           console.log('item data',mapData.response.venues[layer.options.idx])
+    //            map.setView(layer.getLatLng(), 14);
+    //            layer.openPopup();
+    //            // kills the link functionality so it doesn't just to the top
+    //            // of the page
+    //            return false;
+    //         };
+    //     });
+return foursquarePlaces;
 }
 $('.searchBtn').on('click', function(e) {
     e.preventDefault();
     var searchTerm = $('#restaurant').val();
-    $('.jumbotron').html('');
+async.series([
+    function(callback){
+
+    if (map) $('#map').remove();
+    $('div#site-container.container').append(
+    '<div class="jumbotron" id="map"></div><div class="listings"></div>')
+        // do some stuff ...
+        callback(null, 'one');
+    },
+
+    function(callback){
     $.getJSON('/results?what=' + searchTerm, function(searchData) {
 
         console.log(searchData);
-    drawMap(searchData);
+        drawMap(searchData);
+        // do some more stuff ...
+        callback(null, 'two');
+    })
+    }
+],
+function(err, results){
+    // results is now equal to ['one', 'two']
+});
 })
-})
+
+// optional callback
   });
 
 //$('.myform').on('submit'...)
