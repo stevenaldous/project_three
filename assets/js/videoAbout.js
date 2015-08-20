81f478a3e5c567962e55d4c297a695b19c862ef7
scruffmuffler <sloan.coffin@gmail.com>
2015-08-20 09:41:55 -0700

    Cleaned up front end dependencies

---
+++ assets/js/videoAbout.js
@@ -0,0 +1,14 @@
+// $().ready(function() {
+//     $('#site-container').tubular({videoId: 'vbVvF5wWqf0'}); // where idOfYourVideo is the YouTube ID.
+// });
+$().ready(function() {
+  var pathname = window.location.pathname;
+  console.log(pathname);
+  console.log("where is video?");
+  if (pathname ==='/about') {
+    var BV = new $.BigVideo();
+
+    BV.init();
+    BV.show('http://vjs.zencdn.net/v/oceans.mp4',{ambient:true});
+  }
+});
